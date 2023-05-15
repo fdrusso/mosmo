4bd2f7d62a4c5a9f9f21b4401782b65929f95aa1
Frank Russo <fdrusso.42@gmail.com>
2023-05-15 10:44:05 -0700

    Add tests for pack() and unpack()

--- tests/model/reaction_network_test.py
+++ tests/model/reaction_network_test.py
@@ -1,6 +1,8 @@
 """Tests for mosmo.model.reaction_network."""
 from dataclasses import dataclass
 
+import numpy as np
+
 from mosmo.model.base import KbEntry
 from mosmo.model.core import Molecule, Reaction
 from mosmo.model.reaction_network import Index, ReactionNetwork
@@ -17,40 +19,51 @@ class _Tidbit(KbEntry):
         return hash((type(self), self.id))
 
 
+T1 = _Tidbit("t1", description="Your mother was a hamster")
+T2 = _Tidbit("t2", description="Your father smelt of elderberries")
+T3 = _Tidbit("t3", description="We already have one")
+
+
 class TestIndex:
     def test_ListLike(self):
         """Tests the list-like behavior of Index."""
-        x = _Tidbit("x", description="Your mother was a hamster")
-        y = _Tidbit("y", description="Your father smelt of elderberries")
-        tidbits = Index[_Tidbit]([x, y])
+        tidbits: Index[_Tidbit] = Index([T1, T2])
         assert len(tidbits) == 2
-        assert x in tidbits
+        assert T1 in tidbits
         for i, tidbit in enumerate(tidbits):
             assert tidbit is tidbits[i]
-        assert tidbits.index_of(y) == 1
+        assert tidbits.index_of(T2) == 1
 
     def test_SetLike(self):
         """Tests the set-like behavior of Index."""
-        x = _Tidbit("x", description="Your mother was a hamster")
-        y = _Tidbit("y", description="Your father smelt of elderberries")
-        z = _Tidbit("z", description="We already have one")
-
-        tidbits = Index[_Tidbit]()
+        tidbits: Index[_Tidbit] = Index()
         assert len(tidbits) == 0
 
-        tidbits.add(x)
+        tidbits.add(T1)
         assert len(tidbits) == 1
-        assert x in tidbits
-        assert y not in tidbits
+        assert T1 in tidbits
+        assert T2 not in tidbits
 
-        tidbits.add(y)
-        tidbits.add(x)  # No-op by set semantics
+        tidbits.add(T2)
+        tidbits.add(T1)  # No-op by set semantics
         assert len(tidbits) == 2
-        assert tidbits[-1] is y
+        assert tidbits[-1] is T2
 
-        tidbits.update([y, z])
+        tidbits.update([T2, T3])
         assert len(tidbits) == 3
-        assert tidbits.index_of(z) == 2
+        assert tidbits.index_of(T3) == 2
+
+    def test_Pack(self):
+        tidbits: Index[_Tidbit] = Index([T1, T2, T3])
+        v = tidbits.pack({T2: 3}, default=-1)
+        assert np.all(v == np.array([-1, 3, -1]))
+
+    def test_Unpack(self):
+        tidbits: Index[_Tidbit] = Index([T2, T3, T1])  # Changed order
+        data = tidbits.unpack(np.array([3.14159, 2.71828, 1.618]))
+        assert data[T1] == 1.618
+        assert data[T2] == 3.14159
+        assert data[T3] == 2.71828
 
 
 ABCD = Reaction("abcd", stoichiometry={Molecule("a"): -1, Molecule("b"): -2, Molecule("c"): 2, Molecule("d"): 1})
